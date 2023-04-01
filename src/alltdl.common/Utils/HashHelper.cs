be2f922e82a3a9e609787e36f24bec95e5609868
alltdl <alltdl@gmail.com>
2023-04-01 04:42:16 -0600

    Added Md5hash helper

---
+++ src/alltdl.common/Utils/HashHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace alltdl.Utils
+{
+    public static class HashHelper
+    {
+        // Hash an input string and return the hash as
+        // a 32 character hexadecimal string.
+        public static string ConvertToMd5Hash(string input)
+        {
+            // Create a new instance of the MD5CryptoServiceProvider object.
+            MD5 md5Hasher = MD5.Create();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(
+                Encoding.Default.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString(); // Return the hexadecimal string.
+        }
+
+        // Verify a hash against a string.
+        public static bool VerifyMd5Hash(string input, string hash)
+        {
+            string hashOfInput = ConvertToMd5Hash(input); // Hash the input.
+
+            //Create a StringComparer an comare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
