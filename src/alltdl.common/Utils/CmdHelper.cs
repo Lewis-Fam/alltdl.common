3f71eb713aae4c6db74c369d0c5d7d72887f1b95
Lewis-Fam <54489901+Lewis-Fam@users.noreply.github.com>
2024-04-05 23:03:19 -0600

    SP

--- src/alltdl.common/Utils/CmdHelper.cs
+++ src/alltdl.common/Utils/CmdHelper.cs
@@ -1,22 +1,39 @@
-﻿using System.Diagnostics;
+﻿//using System.Diagnostics;
 using System.IO;
 using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
 
 namespace alltdl.Utils
 {
-    public static class CmdHelper
+    public static class CmdLineUtil
     {
-        public static string RunCommand(string commandToRun, string workingDirectory = null)
+        public static string ExecuteCurlDownload(string outputFile, string download)
+        {
+            var cmd =
+                $"curl.exe -o {outputFile} \"{download}\"";
+
+            Debug.WriteLine($"Executing Curl: {cmd}");
+
+            return ExecuteCommand(cmd);
+        }
+
+        public static string ExecuteCommand(string commandToRun, string? workingDirectory = null, int milliseconds = 300000, bool redirectOutput = true)
         {
             if (string.IsNullOrEmpty(workingDirectory))
             {
-                workingDirectory = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());
+                //workingDirectory = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());
+                workingDirectory = Directory.GetCurrentDirectory();
+                Debug.WriteLine($"Working Directory={workingDirectory}");
             }
 
             var processStartInfo = new ProcessStartInfo()
             {
                 FileName = "cmd",
-                RedirectStandardOutput = true,
+                RedirectStandardOutput = redirectOutput,
                 RedirectStandardInput = true,
                 WorkingDirectory = workingDirectory
             };
@@ -28,10 +45,60 @@ namespace alltdl.Utils
             }
 
             process.StandardInput.WriteLine($"{commandToRun} & exit");
-            var output = process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
+
+            var output = string.Empty;
+
+            if (redirectOutput)
+                output = process.StandardOutput.ReadToEnd();
+
+            process.WaitForExit(milliseconds);
 
             return output;
         }
+
+        public static async Task<string> RunCommandAsync(string commandToRun, string? workingDirectory = null, int milliseconds = 300000,
+            CancellationToken cancellationToken = default, bool redirectOutput = true)
+        {
+            if (string.IsNullOrEmpty(workingDirectory))
+            {
+                workingDirectory = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());
+            }
+
+            var processStartInfo = new ProcessStartInfo()
+            {
+                FileName = "cmd",
+                RedirectStandardOutput = redirectOutput,
+                RedirectStandardInput = true,
+                WorkingDirectory = workingDirectory
+            };
+
+            var process = Process.Start(processStartInfo);
+            if (process == null)
+            {
+                throw new Exception("Process should not be null.");
+            }
+
+            var output = string.Empty;
+
+            try
+            {
+                await process.StandardInput.WriteLineAsync($"{commandToRun} & exit");
+
+                if (redirectOutput)
+                    output = await process.StandardOutput.ReadToEndAsync();
+
+                //await process.WaitForExitAsync(cancellationToken);
+                process.WaitForExit(milliseconds);
+
+            }
+            catch (TaskCanceledException e)
+            {
+                output = e.Message;
+            }
+
+            return output;
+        }
+
+        
     }
 }
---
+++ src/alltdl.common/Utils/HttpHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alltdl.Utils
+{
+    public class HttpHelper
+    {
+        public static async Task DownloadFileAsync(string outputFileName, string downloadUrl)
+        {
+            try
+            {
+                using var client = new HttpClient();
+                using var request = new HttpRequestMessage(HttpMethod.Get, downloadUrl);
+                using var sendTask = await client.GetAsync(downloadUrl);
+                using var response = sendTask.EnsureSuccessStatusCode();
+                //var cl = response.Content.Headers.ContentLength.GetValueOrDefault();        
+                Console.WriteLine("StatusCode: " + response.StatusCode);
+                await using var httpStream = await client.GetStreamAsync(downloadUrl);
+                await using var fileStream = File.Create(outputFileName);
+                using var reader = new StreamReader(httpStream);
+                await httpStream.CopyToAsync(fileStream);
+                client.Dispose();
+                request.Dispose();
+                sendTask.Dispose();
+                response.Dispose();
+                fileStream.Flush();
+                reader.Dispose();
+                await httpStream.FlushAsync();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                System.Console.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
